db9f6367cdf2cbb8a30822ecae1941c113b8baa8
Darian Low <dlow3@jhu.edu>
2018-08-23 16:46:31 -0700

    outlined next steps, just need to figure out syntax for fastest way to calculate distance between two np arrays

--- SIFT/FRSIFT.py
+++ SIFT/FRSIFT.py
@@ -25,8 +25,27 @@ dictionary = BOW.cluster()
 
 print("Saving Cluster")
 
-np.savetxt("dictionary.py", dictionary)
+#np.savetxt("dictionary.py", dictionary)
 
+
+#TODO make this not repetitive
+for root, dirs, files in os.walk('data'):
+	for name in files:
+		clusterIDs = []
+		dist = 100000000000000000
+		image = os.path.join(root, name)
+		
+		if (image.lower().endswith('.jpg')):
+			img = cv.imread(image)
+			gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+			
+			kp, dsc = sift.detectAndCompute(img, None)
+		
+		for points in dsc:
+			#compare each descriptor to each cluster id
+			#find the min distance between the two most likely using np.linalg.norm(point-dictionary)
+			#set the min dist cluster as the ID for the descriptor
+			#replacing the 128 dimension vector with one number
 
 """
 with open('dictionary.py', 'w') as file:
